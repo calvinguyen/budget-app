b4cd731e6410360e2ea59ae670f1afda092da57e
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-05-15 14:18:12 -0500

    Add login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Dashboard, { dashBoardLoader } from "./views/Dashboard";
-import Error from "./views/Error";
-import Main, { mainLoader } from "./layouts/Main";
-import { logoutAction } from "./actions/logout";
+import Dashboard, { dashBoardAction, dashBoardLoader } from './views/Dashboard';
+import Error from './views/Error';
+import Main, { mainLoader } from './layouts/Main';
+import { logoutAction } from './actions/logout';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Main />,
     loader: mainLoader,
+    errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Dashboard />,
         loader: dashBoardLoader,
+        action: dashBoardAction,
+        errorElement: <Error />,
       },
       {
         path: 'logout',
-        action: logoutAction
-      }
-    ]
+        action: logoutAction,
+        errorElement: <Error />,
+      },
+    ],
   },
   {
-    path: "*",
-    element: <Error />
-  }
+    path: '*',
+    element: <Error />,
+  },
 ]);
 
 function App() {
-
   return (
     <div className="App">
       <RouterProvider router={router} />
       <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,19 +1,28 @@
 import { useLoaderData } from 'react-router-dom';
 import { fetchData } from '../helpers';
+import Intro from '../components/Intro';
+import { toast } from 'react-toastify';
 
 export function dashBoardLoader() {
   const userName = fetchData('userName');
   return { userName };
 }
 
+export async function dashBoardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem('userName', JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`)
+  } catch (error) {
+    throw new Error("There was a problem creating your account.")
+  }
+  
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 export default Dashboard;
