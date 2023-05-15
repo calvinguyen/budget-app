b4fe5045c8cd6371a06311f62efb4a0ce23c8444
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-05-15 12:03:29 -0500

    Add eslint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,10 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['react-app', 'prettier'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['prettier'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    semi: 'warn',
   },
-}
+};
