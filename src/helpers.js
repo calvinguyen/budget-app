eda17ed2612721f224d7f9ee5eaa91f24fda4077
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-05-16 13:26:55 -0500

    Add budgetItem cards

--- src/helpers.js
+++ src/helpers.js
@@ -29,7 +29,7 @@ export const createExpense = ({ name, amount, budgetId }) => {
     name: name,
     createdAt: Date.now(),
     amount: +amount,
-    budgetId: budgetId
+    budgetId: budgetId,
   };
   const existingExpenses = fetchData('expenses') ?? [];
   return localStorage.setItem(
@@ -41,3 +41,29 @@ export const createExpense = ({ name, amount, budgetId }) => {
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key);
 };
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData('expenses') ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    if (expense.budgetId !== budgetId) return acc;
+
+    return (acc += expense.amount);
+  }, 0);
+  return budgetSpent;
+};
+
+// Formatting
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: 'currency',
+    currency: 'USD',
+  });
+};
+
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: 'percent',
+    minimumFractionDigits: 0,
+  })
+}
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -4,6 +4,7 @@ import Intro from '../components/Intro';
 import { toast } from 'react-toastify';
 import AddBudgetForm from '../components/AddBudgetForm';
 import AddExpenseForm from '../components/AddExpenseForm';
+import BudgetItem from '../components/BudgetItem';
 
 export function dashBoardLoader() {
   const userName = fetchData('userName');
@@ -37,14 +38,14 @@ export async function dashBoardAction({ request }) {
       throw new Error('There was a problem creating your budget.');
     }
   }
-  
+
   // expense submission
   if (_action === 'createExpense') {
     try {
       createExpense({
         name: values.newExpense,
         amount: values.newExpenseAmount,
-        budgetId: values.newExpenseBudget
+        budgetId: values.newExpenseBudget,
       });
       return toast.success(`Expense ${values.newExpense} created!`);
     } catch (error) {
@@ -64,24 +65,26 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {
-              budgets && budgets.length > 0 ? (
-                <div className="grid-lg">
-                  <div className="flex-lg">
-                    <AddBudgetForm />
-                    <AddExpenseForm budgets={budgets} />
-                  </div>
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
                 </div>
-              ) : (
-                  <div className="grid-sm">
-                    <p>
-                      Personal budgeting is the secret to financial freedom.
-                    </p>
-                    <p>Create a budget to get started!</p>
-                    <AddBudgetForm />
+                <h2>Existing Budgets</h2>
+                <div className="budgets">
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
                 </div>
-              )
-            }
+              </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secret to financial freedom.</p>
+                <p>Create a budget to get started!</p>
+                <AddBudgetForm />
+              </div>
+            )}
           </div>
         </div>
       ) : (
